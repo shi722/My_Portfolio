524912d10cfdc4486f882aad9687d7d0019d1487
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-07-26 17:06:17 +0530

    Changes

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -4,7 +4,7 @@ function NavbarLogo() {
     return ( <div>
 
         <div className='text-xl sm:text-2xl lg:text-3xl font-semibold pb-1'
-        >𝕾𝖍𝖎𝖛𝖆𝖓𝖘𝖍𝖚 𝕾𝖔𝖑𝖆𝖓𝖐𝖎</div>
+        >Shivanshu Solanki</div>
     </div>
     )
 }
